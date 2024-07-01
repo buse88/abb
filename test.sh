664f2fb4c5327826acc6333099c9a4eb3f2febb2
buse88 <79825350+buse88@users.noreply.github.com>
2024-07-01 19:33:26 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,29 +1,52 @@
 #!/bin/bash
 
-# 定义绿色文字的 ANSI 转义序列
+# ANSI颜色码定义
 GREEN='\033[0;32m'
-NC='\033[0m' # 没有颜色
+NC='\033[0m' # No Color
 
 # 提示用户选择操作
-echo "请选择操作："
-echo -e "${GREEN}1. 安装 V2 和 V2内核${NC}"
-echo -e "${GREEN}2. 卸载 V2 和 V2内核${NC}"
-echo -e "${GREEN}3. 清空 V2用户名密码${NC}"
+echo -e "${GREEN}请选择操作：${NC}"
+echo -e "${GREEN}1. 安装 V2RayA 和 V2Ray${NC}"
+echo -e "${GREEN}2. 卸载 V2RayA 和 V2Ray${NC}"
+echo -e "${GREEN}3. 更改 V2RayA 密码${NC}"
 read -p "输入选择的编号 (1/2/3): " choice
 
 if [ "$choice" == "1" ]; then
     # 安装 V2RayA 和 V2Ray
-    echo -e "${GREEN}正在安装 V2RayA 和 V2Ray...${NC}"
+    echo "正在安装 V2RayA 和 V2Ray..."
     wget -qO - https://apt.v2raya.org/key/public-key.asc | sudo tee /etc/apt/keyrings/v2raya.asc
     echo "deb [signed-by=/etc/apt/keyrings/v2raya.asc] https://apt.v2raya.org/ v2raya main" | sudo tee /etc/apt/sources.list.d/v2raya.list
     sudo apt update
     sudo apt install -y v2raya v2ray
     sudo systemctl start v2raya.service
-    echo -e "${GREEN}安装完成。${NC}"
+    echo "安装完成。"
+
+    # 提示用户完成代理设置
+    read -p "是否已完成代理设置？(输入 'y' 完成): " proxy_setup
+    if [ "$proxy_setup" == "y" ]; then
+        # 提示用户选择要安装的版本
+        echo -e "${GREEN}请选择要安装的版本：${NC}"
+        echo -e "${GREEN}1. 5.4${NC}"
+        echo -e "${GREEN}2. 6.1${NC}"
+        read -p "输入选择的版本编号 (1/2): " kernel_choice
+
+        # 根据用户选择替换 KERNEL=
+        if [ "$kernel_choice" == "1" ]; then
+            kernel_version="5.4"
+        elif [ "$kernel_choice" == "2" ]; then
+            kernel_version="6.1"
+        else
+            echo "无效的选择。请重新运行脚本并选择 1 或 2。"
+            exit 1
+        fi
+
+        # 执行代理设置命令，并替换 KERNEL=
+        wget -O - https://www.openmptcprouter.com/server/debian-x86_64.sh |  KERNEL="$kernel_version" sh
+    fi
 
 elif [ "$choice" == "2" ]; then
     # 卸载 V2RayA 和 V2Ray
-    echo -e "${GREEN}正在卸载 V2RayA 和 V2Ray...${NC}"
+    echo "正在卸载 V2RayA 和 V2Ray..."
     sudo systemctl stop v2raya.service
     sudo apt remove --purge -y v2raya v2ray
     sudo rm /etc/apt/sources.list.d/v2raya.list
@@ -34,18 +57,17 @@ elif [ "$choice" == "2" ]; then
     sudo rm -rf ~/.config/v2raya
     sudo rm -rf ~/.config/v2ray
     sudo apt update
-    echo -e "${GREEN}卸载完成。${NC}"
+    echo "卸载完成。"
 
     # 删除自身
-    echo -e "${GREEN}删除脚本自身...${NC}"
+    echo "删除脚本自身..."
     rm -- "$0"
 
 elif [ "$choice" == "3" ]; then
     # 更改 V2RayA 密码
-    sudo v2raya --reset-password 
-    echo -e "${GREEN}密码更改完成正在重启V2。${NC}"
-    sudo systemctl restart v2raya.service
-
+    read -p "请输入新密码: " new_password
+    sudo v2raya config set --key web.password --value "$new_password"
+    echo "密码更改完成。"
 
 else
     echo "无效的选择。请运行脚本并选择 1, 2 或 3。"
