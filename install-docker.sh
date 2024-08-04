d4ebbcab3cedba77bbf0518e18a1161983881ced
buse88 <79825350+buse88@users.noreply.github.com>
2024-08-04 17:47:18 +0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,33 +1,28 @@
 # debian国内源安装docker
 
-# 更新索引包
+echo "更新依赖..."
 sudo apt update
-# 安装设置仓库必须的工具
-echo "安装设置仓库必须的工具..."
-sudo apt update && sudo apt install -y ca-certificates curl gnupg lsb-release
-# 安装HTTPS支持包
-echo "安装HTTPS支持包..."
-sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
-# 添加软件源的GPG密钥
-echo "添加阿里源的GPG密钥..."
-sudo curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/aliyun-docker.gpg
-# 添加Docker软件源
-echo "添加清华 TUNA 的国内源..."
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-ce.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian $(lsb_release -sc) stable" > /etc/apt/sources.list.d/docker.list > /dev/null
-# 安装
-echo "升级软件源..."
-sudo apt update
-echo "安装docker-ce..."
-sudo apt install -y docker-ce
-# docker启动后台服务
+
+echo "安装必要依赖..."
+sudo apt install curl
+sudo apt install software-properties-common
+
+echo "添加官方密钥..."
+curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
+
+echo "安装docker源镜像..."
+sudo add-apt-repository "https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable"
+
+echo "安装docker..."
+sudo apt install docker-ce
+
 echo "启动docker..."
-sudo service docker start
-sudo systemctl restart docker
-sudo systemctl start docker
-echo "查看 Docker 服务状态"
-sudo systemctl status docker
-echo "开机启动docker..."
+systemctl start docker
+#查看docker启动情况
+systemctl status docker
+
+echo "设置开机启动docker..."
 sudo systemctl enable docker
-echo "docker安装完毕"
+echo "docker设置完毕"
 
 
