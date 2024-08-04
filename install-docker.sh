1bf0e8a328484b36441532005e557aedaaa92aa4
buse88 <79825350+buse88@users.noreply.github.com>
2024-08-04 16:58:54 +0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -3,23 +3,33 @@
 # 更新索引包
 sudo apt update
 # 安装设置仓库必须的工具
+echo "安装设置仓库必须的工具..."
 sudo apt update && sudo apt install -y ca-certificates curl gnupg lsb-release
 # 安装HTTPS支持包
+echo "安装HTTPS支持包..."
 sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
 # 添加软件源的GPG密钥
+echo "添加软件源的GPG密钥..."
 sudo curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/aliyun-docker.gpg
 # 添加Docker软件源
+echo "添加Docker软件源..."
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/aliyun-docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 # 安装
+echo "升级软件源..."
 sudo apt update
+echo "安装docker-ce..."
 sudo apt install -y docker-ce
-# 启动docker
+# docker启动后台服务
+echo "启动docker..."
 sudo service docker start
-# 重启docker服务
 sudo systemctl restart docker
-# docker设置开机启动
 sudo systemctl start docker
+echo "查看 Docker 服务状态"
+sudo systemctl status docker
+echo "开机启动docker..."
+sudo systemctl enable docker
+echo "docker安装完毕"
 
 
