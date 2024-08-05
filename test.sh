ae8db3a7bcc5bae4e9bafa84f4ebe4463270a7a0
buse88 <79825350+buse88@users.noreply.github.com>
2024-08-05 18:51:54 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,7 +9,6 @@ BLUE='\033[0;34m'   # 蓝色
 NC='\033[0m'        # 无颜色
 
 # 提示用户选择操作前的颜色提示
-echo -e "${RED}----------1和3二选一即可，不用都安装----------${NC}"
 
 # 处理 curl: (6) Could not resolve host 错误的函数
 resolve_dns_issue() {
@@ -35,6 +34,7 @@ execute_command() {
 
 # 安装 V2RayA 的函数
 install_v2raya() {
+    echo -e "${RED}----------V2和Host 二选一即可，不用都安装----------${NC}"
     echo -e "${GREEN}请选择 V2RayA 和 V2Ray 的安装方式：${NC}"
     echo -e "${GREEN}1. 源在线安装${NC}"
     echo -e "${GREEN}2. 本地或github安装${NC}"
@@ -89,162 +89,6 @@ install_v2raya() {
         # 执行代理设置命令，并替换 KERNEL=
         execute_command "wget -O - https://www.openmptcprouter.com/server/debian-x86_64.sh | KERNEL=\"$kernel_version\" sh"
     fi
-}
-
-# 卸载 V2RayA 的函数
-uninstall_v2raya() {
-    echo "正在卸载 V2RayA 和 V2Ray..."
-    sudo systemctl stop v2raya.service
-    sudo apt remove --purge -y v2raya v2ray
-    sudo rm /etc/apt/sources.list.d/v2raya.list
-    sudo rm /etc/apt/keyrings/v2raya.asc
-    sudo rm -rf /etc/v2raya
-    sudo rm -rf /etc/v2ray
-    sudo rm -rf /usr/local/etc/v2ray
-    sudo rm -rf ~/.config/v2raya
-    sudo rm -rf ~/.config/v2ray
-    sudo apt update
-    echo "卸载完成。"
-    echo "删除脚本自身..."
-    rm -- "$0"
-}
-
-# 修改 HOST 的函数
-modify_host() {
-    # 备份 /etc/hosts 文件
-    if [ ! -f /etc/host_back ]; then
-        sudo cp /etc/hosts /etc/host_back
-        echo "Backup created: /etc/host_back"
-    else
-        echo "Backup already exists: /etc/host_back"
-    fi
-
-    # 修改 /etc/hosts 文件
-    if ! command -v curl &> /dev/null; then
-        echo -e "${RED}检测到 没有安装curl，正在安装...${NC}"
-        echo "安装 curl 命令..."
-        sudo apt-get install -y curl
-    fi
-    execute_command "sudo sh -c 'sed -i \"/# GitHub520 Host Start/Q\" /etc/hosts && curl https://raw.hellogithub.com/hosts >> /etc/hosts'"
-    echo "/etc/hosts 文件修改完成..."
-
-    # 创建定时任务文件
-    local cron_file="/tmp/cron_job"
-    local cron_job="0 * * * * /usr/bin/curl -s https://raw.hellogithub.com/hosts >> /etc/hosts; echo \$(date) >> /tmp/cron_count.txt"
-
-    # 清理任何已有的定时任务
-    (crontab -l 2>/dev/null | grep -v "curl -s https://raw.hellogithub.com/hosts" | crontab -)
-
-    # 设置定时任务
-    echo "$cron_job" > $cron_file
-    crontab $cron_file
-
-    echo -e "${GREEN}定时任务已设置，每小时运行一次，总共更新2次host后，自动关闭更新。${NC}"
-
-    # 检查是否已安装 at 命令
-    if ! command -v at &> /dev/null; then
-        echo -e "${RED}检测到 没有安装at，正在安装...${NC}"
-        echo "安装 at 命令..."
-        sudo apt-get install -y at
-    fi
-
-    # 使用 at 命令在 2 小时后移除定时任务
-    echo '(crontab -l 2>/dev/null | grep -v "curl -s https://raw.hellogithub.com/hosts" | crontab -)' | at now + 2 hours
-
-    # 返回选择页面
-    return_to_menu
-}
-
-# 安装 Docker 的函数
-install_docker() {
-    echo "更新依赖..."
-    sudo apt update
-
-    echo "安装必要依赖..."
-    sudo apt install -y curl software-properties-common
-
-    echo "添加官方密钥..."
-    curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
-
-    echo "安装 Docker 源镜像..."
-    sudo add-apt-repository "https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable"
-
-    echo "安装 Docker 与 Docker Compose..."
-    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-    echo "启动 Docker..."
-    sudo systemctl start docker
-    # 查看 Docker 启动情况
-    sudo systemctl status docker
-    echo "设置开机启动 Docker..."
-    sudo systemctl enable docker
-    echo "Docker 设置完毕"
-
-    docker version
-    docker-compose version
-
-    # 创建目录（如果不存在） /etc/docker/daemon.json
-    sudo mkdir -p /etc/docker  
-
-    # 提示用户输入 Docker 反向代理地址
-    echo -n "请输入 Docker 代理地址，末尾带上 /（如果有多个，用逗号分隔）: "
-    read registry_mirrors
-
-    # 处理用户输入，确保每个地址都带有双引号
-    IFS=',' read -ra ADDR <<< "$registry_mirrors"
-    formatted_mirrors=$(printf '"%s",' "${ADDR[@]}")
-    formatted_mirrors=${formatted_mirrors%,} # 去掉最后一个逗号
-
-    # 将用户输入写入 daemon.json 文件
-    sudo tee /etc/docker/daemon.json <<-EOF
-    {
-      "registry-mirrors": [${formatted_mirrors}]  
-      } 
-EOF
-
-    sudo systemctl daemon-reload  
-    sudo systemctl restart docker
-}
-
-# 安装 SRT 的函数
-install_srt() {
-    echo -e "${GREEN}请选择要安装的版本：${NC}"
-    echo -e "${GREEN}1. 5.4${NC}"
-    echo -e "${GREEN}2. 6.1${NC}"
-    read -p "输入选择的版本编号 (1/2): " kernel_choice
-
-    if [ "$kernel_choice" == "1" ]; then
-        kernel_version="5.4"
-    elif [ "$kernel_choice" == "2" ]; then
-        kernel_version="6.1"
-    else
-        echo "无效的选择。请重新运行脚本并选择 1 或 2。"
-        return
-    fi
-
-    execute_command "wget -O - https://www.openmptcprouter.com/server/debian-x86_64.sh | KERNEL=\"$kernel_version\" sh"
-    echo "安装完毕，请重启服务器，重启后端口为：65222"
-}
-
-# 安装 SRS 的函数
-install_srs() {
-    # 检查 Docker 是否已安装
-    if ! command -v docker &> /dev/null; then
-        echo -e "${RED}Docker 未安装，正在安装 Docker...${NC}"
-        install_docker
-    fi
-
-    echo "正在安装 SRS..."
-    docker run --restart always -d -it --name oryx0 -v $HOME/data0:/data \
-      -p 2021:2022 -p 1935:1935 -p 8088:8000/udp -p 10081:10080/udp \
-      ossrs/oryx:5
-
-    if [ $? -eq 0 ]; then
-        echo "SRS 安装并启动成功。后台地址 http://ip:2021 "
-    else
-        echo -e "${RED}SRS 安装或启动失败。${NC}"
-    fi
-
     # 返回选择页面
     return_to_menu
 }
@@ -279,41 +123,211 @@ change_sources() {
     # 更新软件包列表
     echo "正在更新软件包列表..."
     sudo apt update
-
+    sudo apt upgrade -y
     echo "软件源更换完成。"
+    # 返回选择页面
+    return_to_menu
 }
+
+# 卸载 V2RayA 的函数
+uninstall_v2raya() {
+    echo "正在卸载 V2RayA 和 V2Ray..."
+    sudo systemctl stop v2raya.service
+    sudo apt remove --purge -y v2raya v2ray
+    sudo rm /etc/apt/sources.list.d/v2raya.list
+    sudo rm /etc/apt/keyrings/v2raya.asc
+    sudo rm -rf /etc/v2raya
+    sudo rm -rf /etc/v2ray
+    sudo rm -rf /usr/local/etc/v2ray
+    sudo rm -rf ~/.config/v2raya
+    sudo rm -rf ~/.config/v2ray
+    sudo apt update
+    echo "卸载完成。"
+    echo "删除脚本自身..."
+    rm -- "$0"
+}
+
+modify_host() {
+    # 备份 /etc/hosts 文件
+    echo -e "${RED}----------提示：V2和Host 二选一即可，不用都安装----------${NC}"
+    if [ ! -f /etc/host_back ]; then
+        sudo cp /etc/hosts /etc/host_back
+        echo "Backup created: /etc/host_back"
+    else
+        echo "Backup already exists: /etc/host_back"
+    fi
+
+    # 修改 /etc/hosts 文件
+    # 检查是否已安装at命令
+    if ! command -v curl &> /dev/null; then
+           echo -e "${RED}检测到 没有安装curl，正在安装...${NC}"
+        echo "安装 at 命令..."
+        sudo apt-get install -y curl
+    fi
+    execute_command "sudo sh -c 'sed -i \"/# GitHub520 Host Start/Q\" /etc/hosts && curl https://raw.hellogithub.com/hosts >> /etc/hosts'"
+    echo "/etc/hosts 文件修改完成..."
+    
+
+    # 创建定时任务文件
+    local cron_file="/tmp/cron_job"
+    local cron_job="0 * * * * /usr/bin/curl -s https://raw.hellogithub.com/hosts >> /etc/hosts; echo \$(date) >> /tmp/cron_count.txt"
+
+    # 清理任何已有的定时任务
+    (crontab -l 2>/dev/null | grep -v "curl -s https://raw.hellogithub.com/hosts" | crontab -)
+
+    # 设置定时任务
+    echo "$cron_job" > $cron_file
+    crontab $cron_file
+
+    echo -e "${GREEN}定时任务已设置，每小时运行一次，总共更新2次host后，自动关闭更新。${NC}"
+
+    # 检查是否已安装at命令
+    if ! command -v at &> /dev/null; then
+           echo -e "${RED}检测到 没有安装at，正在安装...${NC}"
+        echo "安装 at 命令..."
+        sudo apt-get install -y at
+    fi
+
+    # 使用 at 命令在 2 小时后移除定时任务
+    echo '(crontab -l 2>/dev/null | grep -v "curl -s https://raw.hellogithub.com/hosts" | crontab -)' | at now + 2 hours
+
+    # 返回选择页面
+    return_to_menu
+}
+
+
+# 安装 Docker 的函数
+install_docker() {
+    echo "更新依赖..."
+    sudo apt update
+
+    echo "安装必要依赖..."
+    sudo apt install -y curl software-properties-common
+
+    echo "添加官方密钥..."
+    curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
+
+    echo "安装 Docker 源镜像..."
+    sudo add-apt-repository "https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable"
+    expect "\r"
+    send "\r"
+
+    echo "安装 Docker 与 Docker Compose..."
+    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
+
+    echo "启动 Docker..."
+    sudo systemctl start docker
+    # 查看 Docker 启动情况
+    sudo systemctl status docker
+
+    echo "设置开机启动 Docker..."
+    sudo systemctl enable docker
+    echo "Docker 设置完毕"
+
+    docker version
+    docker-compose version
+
+    # 创建目录（如果不存在） /etc/docker/daemon.json
+    sudo mkdir -p /etc/docker  
+
+    # 提示用户输入 Docker 反向代理地址
+    echo -n "请输入 Docker 代理地址，末尾带上 /（如果有多个，用逗号分隔）: "
+    read registry_mirrors
+
+    # 处理用户输入，确保每个地址都带有双引号
+    IFS=',' read -ra ADDR <<< "$registry_mirrors"
+    formatted_mirrors=$(printf '"%s",' "${ADDR[@]}")
+    formatted_mirrors=${formatted_mirrors%,} # 去掉最后一个逗号
+
+    # 将用户输入写入 daemon.json 文件
+    sudo tee /etc/docker/daemon.json <<-EOF
+    {
+      "registry-mirrors": [${formatted_mirrors}]  
+      } 
+EOF
+
+    sudo systemctl daemon-reload  
+    sudo systemctl restart docker
+    # 返回选择页面
+    return_to_menu
+}
+
+# 安装 SRT 的函数
+install_srt() {
+    echo -e "${GREEN}请选择要安装的版本：${NC}"
+    echo -e "${GREEN}1. 5.4${NC}"
+    echo -e "${GREEN}2. 6.1${NC}"
+    read -p "输入选择的版本编号 (1/2): " kernel_choice
+
+    if [ "$kernel_choice" == "1" ]; then
+        kernel_version="5.4"
+    elif [ "$kernel_choice" == "2" ]; then
+        kernel_version="6.1"
+    else
+        echo "无效的选择。请重新运行脚本并选择 1 或 2。"
+        return
+    fi
+
+    execute_command "wget -O - https://www.openmptcprouter.com/server/debian-x86_64.sh | KERNEL=\"$kernel_version\" sh"
+    echo "安装完毕，请重启服务器，重启后端口为：65222"
+    # 返回选择页面
+    return_to_menu
+}
+
+# 安装 SRS 的函数
+install_srs() {
+    # 检查 Docker 是否已安装
+    if ! command -v docker &> /dev/null; then
+        echo -e "${RED}Docker 未安装，正在安装 Docker...${NC}"
+        install_docker
+    fi
+    # 安装并启动 SRS 
+    echo "Docker 已安装，正在安装 SRS..."
+    docker run --restart always -d -it --name oryx0 -v $HOME/data0:/data \
+      -p 2021:2022 -p 1935:1935 -p 8088:8000/udp -p 10081:10080/udp \
+      ossrs/oryx:5
+
+    if [ $? -eq 0 ]; then
+        echo "SRS 安装并启动成功。后台地址http://ip:2021 "
+    else
+        echo -e "${RED}SRS 安装或启动失败。${NC}"
+    fi
+    # 返回选择页面
+    #return_to_menu
+}
+install_srs
 
 # 返回主菜单的函数
 return_to_menu() {
-    echo -e "${BLUE}----------返回主菜单----------${NC}"
     echo -e "${GREEN}请选择操作：${NC}"
-    echo -e "${GREEN}1. 安装 V2RayA${NC}"
-    echo -e "${GREEN}2. 卸载 V2RayA${NC}"
-    echo -e "${GREEN}3. 修改 HOST${NC}"
-    echo -e "${GREEN}4. 安装 SRT${NC}"
-    echo -e "${GREEN}5. 安装 SRS${NC}"
-    echo -e "${GREEN}6. 更换软件源${NC}"
+    echo -e "${GREEN}1. 更换软件源${NC}"
+    echo -e "${GREEN}2. 修改 HOST${NC}"
+    echo -e "${GREEN}3. 安装 SRT${NC}"
+    echo -e "${GREEN}4. 安装 SRS(docker版)${NC}"
+    echo -e "${GREEN}5. 安装 V2${NC}"
+    echo -e "${GREEN}6. 卸载 V2${NC}"
     echo -e "${GREEN}0. 退出${NC}"
     read -p "输入选择的编号 (0-6): " choice
 
+
     case "$choice" in
         1)
-            install_v2raya
+            change_sources
             ;;
         2)
-            uninstall_v2raya
+             modify_host
             ;;
         3)
-            modify_host
-            ;;
-        4)
             install_srt
             ;;
-        5)
+        4)
             install_srs
             ;;
+        5)
+            install_v2raya
+            ;;
         6)
-            change_sources
+            uninstall_v2raya
             ;;
         0)
             echo "退出脚本。"
