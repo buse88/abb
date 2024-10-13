528bf36446da9f39572d1d05ffa1cc58c46ff04a
buse88 <79825350+buse88@users.noreply.github.com>
2024-10-13 13:25:04 +0800

    Delete startall.sh

--- startall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo -e -n "$(/root/screen/openlcd.sh)" >/dev/nul 2>&1
-echo -e -n "$(/root/screen/bps.sh)" >/dev/nul 2>&1
-echo -e -n "$(/root/screen/power.sh)" >/dev/nul 2>&1
-sleep 10
-echo -e -n "n1.val=1\xff\xff\xff" > /dev/ttyUSB0
